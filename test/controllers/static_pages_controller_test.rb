79aa634ed15d131fa8f39f9892f9b763902dca38
Natsumi <723.pepper@gmail.com>
2018-05-18 18:45:48 +0900

    Finish user signup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,25 +6,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
      get root_path
      assert_response :success
-     assert_select "title", "Ruby on Rails Tutorial Sample App"
+     assert_select "title", "OKURIMORI -おくりもり-"
    end
 
    test "should get help" do
      get help_path
      assert_response :success
-     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+     assert_select "title", "Help | OKURIMORI -おくりもり-"
    end
 
    test "should get about" do
      get about_path
      assert_response :success
-     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+     assert_select "title", "About | OKURIMORI -おくりもり-"
    end
 
    test "should get contact" do
      get contact_path
      assert_response :success
-     assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+     assert_select "title", "Contact | OKURIMORI -おくりもり-"
    end
 
 
